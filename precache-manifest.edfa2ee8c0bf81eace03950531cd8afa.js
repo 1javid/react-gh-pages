63b808d6e6eca5e21fdcf9dc5612674d8484bdc3
javidalakbarli <73642700+javidalakbarli@users.noreply.github.com>
2022-12-06 08:55:40 +0400

    Updates

---
+++ precache-manifest.edfa2ee8c0bf81eace03950531cd8afa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfd07117bda6eb8c63f4",
+    "url": "/1javid/react-gh-pages.git/static/js/main.dfd07117.chunk.js"
+  },
+  {
+    "revision": "14fcefedd3940c15128b",
+    "url": "/1javid/react-gh-pages.git/static/js/1.14fcefed.chunk.js"
+  },
+  {
+    "revision": "537be48969d4a592721b",
+    "url": "/1javid/react-gh-pages.git/static/js/runtime~main.537be489.js"
+  },
+  {
+    "revision": "21e7cca0a4d1920721980f69d4acf82d",
+    "url": "/1javid/react-gh-pages.git/index.html"
+  }
+];
---
+++ static/js/main.dfd07117.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,t,n){e.exports=n(4)},function(e,t,n){"use strict";n.r(t);var o=n(0),c=n.n(o),r=n(2);n.n(r).a.createRoot(document.getElementById("root")).render(c.a.createElement("h2",null,"Welcome to first locally created React App"))}],[[3,2,1]]]);
+//# sourceMappingURL=main.dfd07117.chunk.js.map
